9ab8e0113e5b8e06b0422bc66ac95bf58b8e036c
evepupil <1766468434@qq.com>
2025-10-09 11:03:23 +0800

    新增本地化词条

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -115,7 +115,7 @@ export default function Layout({ children }: LayoutProps) {
         <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
           <div className="text-center text-gray-600">
             <p className="text-sm">
-              © 2025 Brain Mark. Made by Yetong for cognitive testing.
+              {t('footer.copyright')}
             </p>
           </div>
         </div>
